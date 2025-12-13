de8b42ca4a6d283832d0438c82dbcd91da69ff88
Affatih <eftehaplusenamdua@gmail.com>
2025-12-13 10:23:58 -0500

    Delete LabWeb11 directory

--- LabWeb11/module/artikel/hapus.php
+++
@@ -1,9 +0,0 @@
-<?php
-$db = new Database();
-$id = $_GET['id'];
-
-$db->query("DELETE FROM artikel WHERE id=$id");
-
-header("Location: /LabWeb11/artikel/index");
-exit;
-?>
--- LabWeb11/tamplate/sidebar.php
+++
@@ -1,6 +0,0 @@
-// slidebar.php
-<div class="sidebar">
-    <h3>Menu</h3>
-    <a href="/LabWeb11/artikel/index">Artikel</a><br>
-    <a href="/LabWeb11/artikel/tambah">Tambah Artikel</a>
-</div>
